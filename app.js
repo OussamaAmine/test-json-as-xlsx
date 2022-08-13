23f6a3d916491789e005ef0f9e51e1b08b3dbd4c
Oussama Amine Bentama <oa.bentama@gmail.com>
2022-08-13 21:08:56 +0100

    new

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const xlsx = require("json-as-xlsx");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const data = [
+  {
+    sheet: "Adults",
+    columns: [
+      { label: "Name", value: "name" },
+      { label: "Age", value: "age", format: '# "years"' },
+    ],
+    content: [
+      { name: "Monserrat", age: 21, more: { phone: "11111111" } },
+      { name: "Luis", age: 22, more: { phone: "12345678" } },
+    ],
+  },
+  {
+    sheet: "Pets",
+    columns: [
+      { label: "Name", value: "name" },
+      { label: "Age", value: "age" },
+    ],
+    content: [
+      { name: "Malteada", age: 4, more: { phone: "99999999" } },
+      { name: "Picadillo", age: 1, more: { phone: "87654321" } },
+    ],
+  },
+];
+
+const settings = {
+  writeOptions: {
+    type: "buffer",
+    bookType: "xlsx",
+  },
+};
+
+app.get("/", (_, res) => {
+  const buffer = xlsx(data, settings);
+  res.writeHead(200, {
+    "Content-Type": "application/octet-stream",
+    "Content-disposition": "attachment; filename=MySheet.xlsx",
+  });
+  res.end(buffer);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
